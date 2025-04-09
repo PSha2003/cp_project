bcbc603320d5dc93ff2a10560a33a0552714b1b0
PSha2003 <160639073+PSha2003@users.noreply.github.com>
2025-04-09 00:34:53 -0400

    Updated tests

--- dlp/tests.py
+++ dlp/tests.py
@@ -2,6 +2,19 @@ from django.test import TestCase
 from .models import Pattern
 
 class PatternTest(TestCase):
+    def setUp(self):
+        self.pattern = Pattern.objects.create(
+            name="CCN",
+            regex=r"\\b\\d{16}\\b",
+        )
     def test_create_pattern(self):
-        pattern = Pattern.objects.create(name="CCN", regex=r"\\b\\d{16}\\b")
-        self.assertEqual(pattern.name, "CCN")
+        #pattern = Pattern.objects.create(name="CCN", regex=r"\\b\\d{16}\\b")
+        self.assertEqual(self.pattern.name, "CCN")
+
+    def test_pattern_matching(self):
+        """Test the regex pattern matching works as expected"""
+        result = self.pattern.matches("1234567812123434")
+        self.assertTrue(result)
+        
+        result = self.pattern.matches("abcd-ef-ghij")
+        self.assertFalse(result)
